baef7338a17e6ada2bcd6478e754bc4ecd429539
dayzi99 <96703573+dayzi99@users.noreply.github.com>
2022-12-21 23:07:09 -0500

    Update SparseMatrixCSC.java

--- app/src/main/java/proyecto/SparseMatrixCSC.java
+++ app/src/main/java/proyecto/SparseMatrixCSC.java
@@ -19,22 +19,99 @@ public class SparseMatrixCSC {
         //Load data
         loader.loadFile(inputFile);
         matrix = loader.getMatrix();
-        throw new OperationNotSupportedException();
+        //Load data
+        loader.loadFile(inputFile);
+        matrix = loader.getMatrix();
+
+        //Creamos nuevos LinkedList para los atributos valor y columna ya que no sabemos el tamaña que tendrán.
+        //Columnas se obtiene de una manera diferente
+        LinkedList<Integer> valor = new LinkedList<Integer>();
+        LinkedList<Integer> fila = new LinkedList<Integer>();
+        this.columns = new int[this.matrix[0].length + 1];
+        int contador = 0;
+
+        //La matriz se recorre de forma inversa, primero las columnas y luego las filas.
+
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[j][i] != 0) {
+                    valor.add(matrix[j][i]);
+                    fila.add(j);
+                    contador++;
+                }
+            }
+            if (i == 0) {
+                this.columns[1] = contador;
+                contador = 0;
+            } else {
+                this.columns[i + 1] = this.columns[i] + contador;
+                contador = 0;
+            }
+        }
+
+        this.rows = new int[fila.size()];
+        this.values = new int[valor.size()];
+
+        int i;
+        i = 0;
+        for (int val: valor) {
+            this.values[i] = val;
+            i++;
+        }
+
+        i = 0;
+        for (int row: fila) {
+            this.rows[i] = row;
+            i++;
+        }
     }
 
     public int getElement(int i, int j) throws OperationNotSupportedException
     {
-        throw new OperationNotSupportedException();
+        for (int z = this.columns[j]; z < this.columns[j + 1]; z++) {
+            if (this.rows[z] == i) {
+                return this.values[z];
+            }
+        }
+        return 0;
     }
 
     public int[] getRow(int i) throws OperationNotSupportedException
     {
-        throw new OperationNotSupportedException();
+        int[] valorFila = new int[this.columns.length - 1];
+        for (int j = 0; j < valorFila.length; j++) {
+            for (int z = this.columns[j]; z < this.columns[j + 1]; z++) {
+                if (this.rows[z] == i) {
+                    valorFila[j] = this.values[z];
+                }
+            }
+        }
+        return valorFila;
     }
-
+    
+    private int max(int[] num) {
+        int numeroMax = num[0];
+        for (int numero : num) {
+            if (numero > numeroMax) {
+                numeroMax = numero;
+            }
+        }
+        return numeroMax;
+    }
+    
     public int[] getColumn(int j) throws OperationNotSupportedException
     {
-        throw new OperationNotSupportedException();
+        int[] valorColumna = new int[max(this.rows) + 1];
+
+        for (int i = 0; i < this.columns.length; i++) {
+            if (i == j) {
+                for (int c = this.columns[i]; c < this.columns[i + 1]; c++) {
+                    valorColumna[this.rows[c]] = this.values[c];
+
+                }
+            }
+        }
+        return valorColumna;
     }
 
     public void setValue(int i, int j, int value) throws OperationNotSupportedException
